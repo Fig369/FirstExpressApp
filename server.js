c907ec5494123969f19f9b17f4a5d7f8594240bf
Fig369 <Figueroa.jorge20@gmail.com>
2022-05-01 23:33:22 -0400

    update files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    console.log("here")
+    res.send("hi")
+})
+
+app.listen(port, () => {
+    console.log(`App running on port: ${port}`)
+})
