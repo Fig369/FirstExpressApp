f2ca33b9b6e4f52442c37a8a1081a4609571cc28
Fig369 <Figueroa.jorge20@gmail.com>
2022-05-03 11:10:56 -0400

    update to package.json
    Type with a comma after list of express dependency that cause a 500 ERROR

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ var app = express();
 var port = 3000;
 
 const movies = {
-    movie1: ` Interstellar `,
-    movie2: ` Inception `,
-    movie3: ` Tenet `,
-    movie4: ` This Is the End `,
-    movie5: ` Ben-Hur 1959 `,
-    movie6: ` Pan's Labyrinth `,
-    movie7: ` LOTR `,
-    movie8: ` Star Wars `,
-    movie9: ` Get-Out `,
-    movie10: ` Wall-E `,
+    movie1: `Interstellar` ,
+    movie2: `Inception` ,
+    movie3: `Tenet` ,
+    movie4: `This Is the End` ,
+    movie5: `Ben-Hur 1959` ,
+    movie6: `Pan's Labyrinth` ,
+    movie7: `LOTR` ,
+    movie8: `Star Wars` ,
+    movie9: `Get-Out` ,
+    movie10: `Wall-E` ,
 };
 
 app.use(express.static('public'));
 
 app.get('/:key', (req, res) => {
-    res.send(movies([req.params.key]));
+    res.send(movies[req.params.key]);
 });
 
 app.listen(port, () => {
-    console.log(`App running on port: ${port}`)
-});
+ console.log(`App listening on port ${port}`)
+})
